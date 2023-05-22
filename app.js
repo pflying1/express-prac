76fb8460a21be2485a279be3a8ca185238f47a8f
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-05-23 08:48:44 +0900

    express로 서버열기

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
-//require를 통해 http를 가져오기
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
 
-const jsonData = fs.readFileSync("./financeDB_testData(samsung).json", "utf-8");
-//financeDB_testData(samsung).json 파일을 읽음
-const samsungData = JSON.parse(jsonData);
-const SSdata = samsungData.samsung;
-
-//samsungData.samsung의 값만을 가져옴
-const samsungDataAll = (property) =>
-  SSdata.map((value) => [value[property]]).join("\n");
-
-const samsungDataObject = {
-  no: samsungDataAll("no"),
-  open: samsungDataAll("open"),
-  high: samsungDataAll("high"),
-  low: samsungDataAll("low"),
-  close: samsungDataAll("close"),
-  volume: samsungDataAll("volume"),
-  day: samsungDataAll("day"),
-};
-//createServer를 이용하여 서버를 생성
-const server = http.createServer((request, response) => {
-  //매개변수 request의 요청방식을 GET으로 요청
-  if (request.method === "GET") {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end(`<!DOCTYPE html>
+app.get("/", (req, res) => {
+  res.send(`<!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
@@ -34,7 +11,7 @@ const server = http.createServer((request, response) => {
       <title>Example Page</title>
     </head>
     <body>
-      <div id="root" style = "width : 100vw; height : 100vh;"> 
+      <div id="root" style="width: 100vw; height: 100vh;">
         <div>NO.${samsungDataObject.no}</div>
         </br>
         <div>OPEN.${samsungDataObject.open}</div>
@@ -52,13 +29,8 @@ const server = http.createServer((request, response) => {
       </div>
     </body>
     </html>`);
-  }
 });
-//서버 포트 대기 요청
-server.listen(3050, (err) => {
-  if (err) {
-  } else {
-    //포트가 연결되었을시, 콘솔로 띄우기
-    console.log("3050 포트 열림");
-  }
+
+app.listen(3050, () => {
+  console.log("3050 포트 열림");
 });
